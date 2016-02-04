6bde4f74ba38603175d5102e1b9a1b5cc9667baf
Winbobob <zhu6@ncsu.edu>
2016-02-04 00:09:58 -0500

    Add data.

--- client_end/ReputationCalculation-Javaclient/src/client/DBQuery.java
+++ client_end/ReputationCalculation-Javaclient/src/client/DBQuery.java
@@ -2,7 +2,7 @@ package client;
 
 public class DBQuery {
 
-	public static String GetTotalScore(int assignment_id)
+	public static String GetTotalScore(int assignment_id, boolean hasTopic)
 	{
 		String query="SELECT U.id, RM.reviewee_id as submission_id, "+
 					"sum(A.answer * Q.weight) / sum(QN.max_question_score * Q.weight) * 100 as total_score "+
@@ -13,15 +13,19 @@ public class DBQuery {
 					"inner join response_maps RM on R.map_id = RM.id  "+
 					"inner join participants P on P.id = RM.reviewer_id "+
 					"inner join users U on U.id = P.user_id "+
-					"inner join Teams T on T.id = RM.reviewee_id "+
-					"inner join signed_up_teams SU_team on SU_team.team_id = T.id "+
-					"where RM.type='ReviewResponseMap'  "+
+					"inner join Teams T on T.id = RM.reviewee_id ";
+					if (hasTopic == true){
+						query+="inner join signed_up_teams SU_team on SU_team.team_id = T.id ";
+					}
+					query+="where RM.type='ReviewResponseMap'  "+
 					"and RM.reviewed_object_id = "+  Integer.toString(assignment_id) + " " +
 					"and A.answer is not null "+
 					"and Q.type ='Criterion' "+
-					"and R.round=2 "+
-					"and SU_team.is_waitlisted = 0 "+
-					"group by RM.id  "+
+					"and R.round=2 ";
+					if (hasTopic == true){
+						query+="and SU_team.is_waitlisted = 0 ";
+					}
+					query+="group by RM.id  "+
 					"order by RM.reviewee_id";
 		return query;
 	}
--- client_end/ReputationCalculation-Javaclient/src/client/JsonClient.java
+++ client_end/ReputationCalculation-Javaclient/src/client/JsonClient.java
@@ -27,7 +27,7 @@ public class JsonClient {
 			DbAdapter dbAdapter = DbAdapter.GetInstance();
 			
 			HashMap<String, HashMap<String, Float>> submissions = 
-					Get_TotalScores(dbAdapter.Get_ResultSet(DBQuery.GetTotalScore(733)));
+					Get_TotalScores(dbAdapter.Get_ResultSet(DBQuery.GetTotalScore(736, true)));
 			
 			String inputText = GenerateInputText(submissions);
 			
@@ -94,12 +94,20 @@ public class JsonClient {
 	}
 	
 	public static String GenerateInputText(HashMap<String, HashMap<String, Float>> submissions){
-		/*String inputText = "{\"expert_grades\": {" +
-						   "\"submission23910\": 86, \"submission23926\":88, \"submission23913\":93," +
-						   "\"submission23907\": 90, \"submission23924\":90, \"submission23906\":95," +
-						   "\"submission23917\": 89, \"submission23908\":82},";*/
 		String inputText = "{";
-		//"{"expert_grades": {"submission1": 90, "submission2":88, "submission3":93},  #optional
+		//expert grades of Wiki 1a and 1b (724, 733)
+		//inputText += "\"expert_grades\": {\"submission23967\":93, \"submission23969\":89, \"submission23971\":95, \"submission23972\":86, \"submission23973\":91, \"submission23975\":94, \"submission23979\":90, \"submission23980\":94, \"submission23981\":87, \"submission23982\":79, \"submission23983\":91, \"submission23986\":92, \"submission23987\":91, \"submission23988\":93, \"submission23991\":98, \"submission23992\":91, \"submission23994\":87, \"submission23995\":93, \"submission23998\":92, \"submission23999\":87, \"submission24000\":93, \"submission24001\":93, \"submission24006\":96, \"submission24007\":87, \"submission24008\":92, \"submission24009\":92, \"submission24010\":93, \"submission24012\":94, \"submission24013\":96, \"submission24016\":91, \"submission24018\":93, \"submission24024\":96, \"submission24028\":88, \"submission24031\":94, \"submission24040\":93, \"submission24043\":95, \"submission24044\":91, \"submission24046\":95, \"submission24051\":92, \"submission24100\":90, \"submission24079\":92, \"submission24298\":86, \"submission24545\":92, \"submission24082\":96, \"submission24080\":86, \"submission24284\":92, \"submission24534\":93, \"submission24285\":94, \"submission24297\":91},";
+		//expert grades of program 1 (735)
+		//inputText += "\"expert_grades\": {\"submission24083\":96.084,\"submission24085\":88.811,\"submission24086\":100,\"submission24087\":100,\"submission24088\":92.657,\"submission24091\":96.783,\"submission24092\":90.21,\"submission24093\":100,\"submission24097\":90.909,\"submission24098\":98.601,\"submission24101\":99.301,\"submission24278\":98.601,\"submission24279\":72.727,\"submission24281\":54.476,\"submission24289\":94.406,\"submission24291\":99.301,\"submission24293\":93.706,\"submission24296\":98.601,\"submission24302\":83.217,\"submission24303\":91.329,\"submission24305\":100,\"submission24307\":100,\"submission24308\":100,\"submission24311\":95.804,\"submission24313\":91.049,\"submission24314\":100,\"submission24315\":97.483,\"submission24316\":91.608,\"submission24317\":98.182,\"submission24320\":90.21,\"submission24321\":90.21,\"submission24322\":98.601},";
+		//initial hamer reputation of Wiki 1a and 1b (724, 733)
+		//inputText += "\"initial_hamer_reputation\":{\"stu5687\":1.267,\"stu5787\":2.15,\"stu5790\":3.426,\"stu5791\":1.48,\"stu5795\":1.121,\"stu5796\":0.643,\"stu5797\":2.159,\"stu5800\":1.269,\"stu5801\":2.659,\"stu5804\":2.16,\"stu5806\":0.809,\"stu5807\":2.096,\"stu5808\":4.223,\"stu5810\":1.034,\"stu5811\":3.765,\"stu5814\":0.931,\"stu5815\":2.504,\"stu5818\":0.257,\"stu5820\":2.477,\"stu5822\":2.31,\"stu5824\":2.113,\"stu5825\":2.856,\"stu5826\":2.296,\"stu5827\":0.438,\"stu5828\":0.729,\"stu5829\":3.388,\"stu5830\":0.893,\"stu5832\":2.551,\"stu5835\":1.579,\"stu5837\":0.578,\"stu5839\":0.743,\"stu5840\":0.996,\"stu5841\":0.506,\"stu5843\":2.432,\"stu5846\":1.633,\"stu5848\":0.757,\"stu5849\":0.299,\"stu5850\":2.272,\"stu5855\":0.334,\"stu5856\":0.672,\"stu5857\":2.415,\"stu5859\":2.427,\"stu5860\":0.627,\"stu5862\":0.884,\"stu5863\":0.723,\"stu5864\":1.534,\"stu5866\":0.505,\"stu5867\":2.2,\"stu5868\":2.005,\"stu5869\":0.755,\"stu5870\":0.252,\"stu5871\":2.144,\"stu5873\":0.528,\"stu5874\":0.923,\"stu5875\":1.974,\"stu5876\":1.33,\"stu5880\":0.782},";
+		//initial hamer reputation of program 1 (735)
+		inputText += "\"initial_hamer_reputation\":{\"stu4381\":2.649, \"stu5415\":3.022, \"stu5687\":3.578, \"stu5787\":3.142, \"stu5788\":2.424, \"stu5789\":0.134, \"stu5790\":2.885, \"stu5792\":2.27, \"stu5793\":2.317, \"stu5794\":2.219, \"stu5795\":1.232, \"stu5796\":0.832, \"stu5797\":2.946, \"stu5798\":0.225, \"stu5799\":5.365, \"stu5800\":2.749, \"stu5801\":4.161, \"stu5802\":4.78, \"stu5803\":0.366, \"stu5804\":0.262, \"stu5805\":3.016, \"stu5806\":0.561, \"stu5807\":3.028, \"stu5808\":3.435, \"stu5810\":3.664, \"stu5812\":2.638, \"stu5813\":2.621, \"stu5814\":3.035, \"stu5815\":2.985, \"stu5816\":0.11, \"stu5817\":2.16, \"stu5818\":0.448, \"stu5821\":0.294, \"stu5822\":1.874, \"stu5823\":3.339, \"stu5824\":3.597, \"stu5825\":4.033, \"stu5826\":2.962, \"stu5827\":1.49, \"stu5828\":3.208, \"stu5830\":1.211, \"stu5832\":0.406, \"stu5833\":3.04, \"stu5836\":3.396, \"stu5838\":4.519, \"stu5839\":2.974, \"stu5840\":1.952, \"stu5843\":3.515, \"stu5844\":0.627, \"stu5845\":2.355, \"stu5846\":3.604, \"stu5847\":3.847, \"stu5848\":1.488, \"stu5849\":2.078, \"stu5850\":2.957, \"stu5851\":2.774, \"stu5852\":2.345, \"stu5853\":1.717, \"stu5854\":2.275, \"stu5855\":2.216, \"stu5856\":1.4, \"stu5857\":3.463, \"stu5858\":3.132, \"stu5859\":3.327, \"stu5860\":0.965, \"stu5861\":1.683, \"stu5862\":1.647, \"stu5863\":0.457, \"stu5864\":3.901, \"stu5866\":2.402, \"stu5867\":1.509, \"stu5868\":0.198, \"stu5869\":1.434, \"stu5870\":0.43, \"stu5871\":0.654, \"stu5872\":0.854, \"stu5873\":2.645, \"stu5874\":1.988, \"stu5875\":0.089, \"stu5876\":3.438, \"stu5878\":3.763, \"stu5880\":2.444, \"stu5881\":0.316},";
+				
+		//Eg.
+		//"{"initial_hamer_reputation": {"stu1": 0.90, "stu2":0.88, "stu3":0.93, "stu4":0.8, "stu5":0.93, "stu8":0.93},  #optional
+		//"initial_lauw_reputation": {"stu1": 1.90, "stu2":0.98, "stu3":1.12, "stu4":0.94, "stu5":1.24, "stu8":1.18},  #optional
+		//"expert_grades": {"submission1": 90, "submission2":88, "submission3":93},  #optional
 		//"submission1": {"stu1":91, "stu3":99},"submission2": {"stu5":92, "stu8":90},"submission3": {"stu2":91, "stu4":88}}"
 		Set<String> submission_ids = submissions.keySet();
 		Iterator<String> submission_iterator = submission_ids.iterator();
